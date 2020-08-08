f905997b725f03573101dbce126dbf7006214609
sockstack <85564748@qq.com>
2020-08-08 20:01:39 +0800

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -5,15 +5,9 @@ import (
 	"cn.sockstack/smser/internal"
 	"cn.sockstack/smser/middlewares"
 	"cn.sockstack/smser/worker"
-	"time"
 )
 
 func main() {
 	worker.Worker.Start()
-	time.Sleep(time.Second)
-	go func() {
-		time.Sleep(time.Second * 3)
-		worker.Worker.Restart()
-	}()
 	internal.NewSmser().Route(api.R).Use(middlewares.RequestLog()).Run()
 }
